5edd8945f159168af91213dc2617a8aa5d589f16
Alexey Shevchenko <ivanovosity@gmail.com>
2017-02-14 19:52:01 +0300

    Correct enable/disable debug panel prod and dev

--- environments/dev/api/config/main-local.php
+++ environments/dev/api/config/main-local.php
@@ -8,7 +8,7 @@ $config = [
     ],
 ];
 
-if (!YII_ENV_TEST) {
+if (!YII_ENV_TEST || YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
