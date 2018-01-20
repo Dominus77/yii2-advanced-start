498c9c432526855a86fd3ede827a77c048fb96b1
Alexey Schevchenko <ivanovosity@gmail.com>
2018-01-20 21:21:58 +0300

    fix defined

--- environments/dev/api/web/index-test.php
+++ environments/dev/api/web/index-test.php
@@ -1,7 +1,8 @@
 <?php
 
 // NOTE: Make sure this file is not accessible when deployed to production
-if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
+$ip = @$_SERVER['REMOTE_ADDR'];
+if (!in_array($ip, ['127.0.0.1', '::1'])) {
     die('You are not allowed to access this file.');
 }
 
