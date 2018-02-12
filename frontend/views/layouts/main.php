5bcd5ffe3006e991279760e027513ca8168b2c86
Alexey Schevchenko <ivanovosity@gmail.com>
2018-02-12 17:46:14 +0300

    fix

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -31,7 +31,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => Html::img('@web/images/logo.png', ['alt' => Yii::$app->name, 'class' => 'yii-logo']). Yii::$app->name, //Yii::$app->name,
+        'brandLabel' => Html::img('@web/images/logo.png', ['alt' => Yii::$app->name, 'class' => 'yii-logo']) . Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
