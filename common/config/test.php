0a562c9d663c7e2a7128908532e472201cb9e8a3
Alexey Schevchenko <ivanovosity@gmail.com>
2018-02-20 15:29:53 +0300

    commit

--- common/config/test.php
+++ common/config/test.php
@@ -12,5 +12,8 @@ return [
             'class' => 'yii\web\User',
             'identityClass' => 'modules\users\models\User',
         ],
+        'request' => [
+            'cookieValidationKey' => 'test',
+        ],
     ],
 ];
