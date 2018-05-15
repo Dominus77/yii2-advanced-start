6d149076bba5a992248fb16d10c5cf8bdfedbef7
Alexey Schevchenko <ivanovosity@gmail.com>
2018-05-15 14:51:43 +0300

    commit

--- modules/users/views/ajax/ajax.js
+++ modules/users/views/ajax/ajax.js
@@ -20,25 +20,3 @@ function handleAjaxLink(e) {
         $link.html(response.body);
     });
 }
-
-/**
- * Ajax Link Generate Auth Key
- * @param e mixed
- */
-function ajaxLinkGenerateAuthKey(e) {
-
-    e.preventDefault();
-
-    var
-        $link = $('#' + this.id),
-        callUrl = $link.attr('href');
-
-    $.ajax({
-        type: "post",
-        dataType: 'json',
-        url: callUrl,
-        data: ({})
-    }).done(function (response) {
-        $('#auth_key_container').html(response.body);
-    });
-}
