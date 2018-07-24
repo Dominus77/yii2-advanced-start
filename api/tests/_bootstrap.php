aef6c04bffb5439beaadffdc260ab549160d462c
Alexey Schevchenko <ivanovosity@gmail.com>
2018-07-24 22:42:03 +0300

    fix api tests

--- api/tests/_bootstrap.php
+++ api/tests/_bootstrap.php
@@ -7,4 +7,4 @@ defined('YII_APP_BASE_PATH') || define('YII_APP_BASE_PATH', __DIR__ . '/../../')
 require_once(YII_APP_BASE_PATH . '/vendor/autoload.php');
 require_once(YII_APP_BASE_PATH . '/vendor/yiisoft/yii2/Yii.php');
 require_once(YII_APP_BASE_PATH . '/common/config/bootstrap.php');
-require_once(__DIR__ . '/../config/bootstrap.php');
+require_once(YII_APP_BASE_PATH . '/api/config/bootstrap.php');
