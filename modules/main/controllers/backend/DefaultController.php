5e43f8d9ee9ad734bbf078f8aa19f15c69208b21
Alexey Schevchenko <ivanovosity@gmail.com>
2018-08-02 04:50:35 +0300

    commit

--- modules/main/controllers/backend/DefaultController.php
+++ modules/main/controllers/backend/DefaultController.php
@@ -48,7 +48,7 @@ class DefaultController extends Controller
         //Greeting in the admin panel :)
         /** @var User $identity */
         $identity = Yii::$app->user->identity;
-        Yii::$app->session->setFlash('primary', Module::t('module', 'Welcome, {:username}!', [
+        Yii::$app->session->setFlash('info', Module::t('module', 'Welcome, {:username}!', [
             ':username' => $identity->username
         ]));
         return $this->render('index');
