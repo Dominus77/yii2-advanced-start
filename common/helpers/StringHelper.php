6a010a509382a94c20832cab6ddd39cffe4a59f0
Alexey Schevchenko <ivanovosity@gmail.com>
2018-08-05 14:56:03 +0300

    add StringHelper

---
+++ common/helpers/StringHelper.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace common\helpers;
+
+/**
+ * Class StringHelper
+ * @package common\helpers
+ */
+class StringHelper extends \yii\helpers\StringHelper
+{
+    /**
+     * Первая буква - заглавная
+     *
+     * @param $string
+     * @param string $e
+     * @return string
+     */
+    public static function ucfirst($string, $e = 'utf-8')
+    {
+        if (function_exists('mb_strtoupper') && function_exists('mb_substr') && !empty($string)) {
+            $string = mb_strtolower($string, $e);
+            $upper = mb_strtoupper($string, $e);
+            preg_match('#(.)#us', $upper, $matches);
+            $string = $matches[1] . mb_substr($string, 1, mb_strlen($string, $e), $e);
+        } else {
+            $string = ucfirst($string);
+        }
+        return $string;
+    }
+
+    /**
+     * Форматирует и конвертирует кол-во байт, возвращает строку
+     *
+     * Например: 9.54 MB
+     *
+     * @param $bytes
+     * @param int $precision
+     * @return string
+     */
+    public static function formatBytes($bytes, $precision = 2)
+    {
+        $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+        $bytes /= pow(1024, $pow);
+        return round($bytes, $precision) . ' ' . $units[$pow];
+    }
+
+    /**
+     * Генерация случайной строки
+     *
+     * @param int $length
+     * @param bool $allowUppercase
+     * @return string
+     */
+    public static function generateRandomString($length = 8, $allowUppercase = true)
+    {
+        $validCharacters = 'abcdefghijklmnopqrstuxyvwz1234567890';
+        if ($allowUppercase) {
+            $validCharacters .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        }
+        $validCharNumber = strlen($validCharacters);
+        $result = '';
+        for ($i = 0; $i < $length; $i++) {
+            $index = mt_rand(0, $validCharNumber - 1);
+            $result .= $validCharacters[$index];
+        }
+        return $result;
+    }
+}
