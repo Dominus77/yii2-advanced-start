de43ce424ab38ef23bd6bcd95ddee5a3c4a35afd
Alexey Schevchenko <ivanovosity@gmail.com>
2018-10-31 20:38:59 +0300

    fix migrate column type

--- modules/rbac/migrations/m170615_234118_create_rbac_table.php
+++ modules/rbac/migrations/m170615_234118_create_rbac_table.php
@@ -87,7 +87,7 @@ class m170615_234118_create_rbac_table extends Migration
 
         $this->createTable($authManager->assignmentTable, [
             'item_name' => $this->string(64)->notNull(),
-            'user_id' => $this->string(64)->notNull(),
+            'user_id' => $this->integer()->notNull(),
             'created_at' => $this->integer(),
             'PRIMARY KEY ([[item_name]], [[user_id]])',
             'FOREIGN KEY ([[item_name]]) REFERENCES ' . $authManager->itemTable . ' ([[name]])' .
