b56569a66c5fcdf55b45428a160abb68a9393c37
Aleksey Schevchenko <ivanovosity@gmail.com>
2019-09-07 15:56:23 +0300

    Refactoring index.php and yii

--- environments/dev/frontend/web/index-test.php
+++ environments/dev/frontend/web/index-test.php
@@ -1,4 +1,8 @@
 <?php
+
+use yii\web\Application;
+use yii\base\InvalidConfigException;
+
 // NOTE: Make sure this file is not accessible when deployed to production
 if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
     die('You are not allowed to access this file.');
@@ -8,9 +12,14 @@ defined('YII_DEBUG') || define('YII_DEBUG', true);
 defined('YII_ENV') || define('YII_ENV', 'test');
 defined('YII_APP_BASE_PATH') || define('YII_APP_BASE_PATH', __DIR__ . '/../../');
 
-require(YII_APP_BASE_PATH . '/vendor/autoload.php');
-require(YII_APP_BASE_PATH . '/vendor/yiisoft/yii2/Yii.php');
-require(YII_APP_BASE_PATH . '/common/config/bootstrap.php');
-require(YII_APP_BASE_PATH . '/frontend/config/bootstrap.php');
+require YII_APP_BASE_PATH . '/vendor/autoload.php';
+require YII_APP_BASE_PATH . '/vendor/yiisoft/yii2/Yii.php';
+require YII_APP_BASE_PATH . '/common/config/bootstrap.php';
+require YII_APP_BASE_PATH . '/frontend/config/bootstrap.php';
 
-(new yii\web\Application($config))->run();
+try {
+    $application = new Application($config);
+} catch (InvalidConfigException $e) {
+}
+
+$application->run();
--- environments/prod/api/web/index.php
+++ environments/prod/api/web/index.php
@@ -1,19 +1,28 @@
 <?php
+
+use yii\helpers\ArrayHelper;
+use yii\web\Application;
+use yii\base\InvalidConfigException;
+
 defined('YII_DEBUG') || define('YII_DEBUG', false);
 defined('YII_ENV') || define('YII_ENV', 'prod');
 defined('YII_APP_BASE_PATH') || define('YII_APP_BASE_PATH', __DIR__ . '/../../');
 
-require(YII_APP_BASE_PATH . '/vendor/autoload.php');
-require(YII_APP_BASE_PATH . '/vendor/yiisoft/yii2/Yii.php');
-require(YII_APP_BASE_PATH . '/common/config/bootstrap.php');
-require(YII_APP_BASE_PATH . '/api/config/bootstrap.php');
+require YII_APP_BASE_PATH . '/vendor/autoload.php';
+require YII_APP_BASE_PATH . '/vendor/yiisoft/yii2/Yii.php';
+require YII_APP_BASE_PATH . '/common/config/bootstrap.php';
+require YII_APP_BASE_PATH . '/api/config/bootstrap.php';
 
-$config = yii\helpers\ArrayHelper::merge(
-    require(YII_APP_BASE_PATH . '/common/config/main.php'),
-    require(YII_APP_BASE_PATH . '/common/config/main-local.php'),
-    require(YII_APP_BASE_PATH . '/api/config/main.php'),
-    require(YII_APP_BASE_PATH . '/api/config/main-local.php')
+$config = ArrayHelper::merge(
+    require YII_APP_BASE_PATH . '/common/config/main.php',
+    require YII_APP_BASE_PATH . '/common/config/main-local.php',
+    require YII_APP_BASE_PATH . '/api/config/main.php',
+    require YII_APP_BASE_PATH . '/api/config/main-local.php'
 );
 
-$application = new yii\web\Application($config);
+try {
+    $application = new Application($config);
+} catch (InvalidConfigException $e) {
+}
+
 $application->run();
