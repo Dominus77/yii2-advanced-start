f347b8796e7edf15d1e683db315309fccdf94971
Aleksey Schevchenko <ivanovosity@gmail.com>
2019-09-07 18:36:53 +0300

    commit

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -7,7 +7,7 @@ return [
     'components' => [
         'db' => [
             'class' => Connection::class,
-            'dsn' => 'mysql:host=localhost;dbname=yii2_advanced_start',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=yii2_advanced_start',
             'username' => 'root',
             'password' => ''
         ],
--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -1,9 +1,16 @@
 <?php
 
+use yii\db\Connection;
 use yii\swiftmailer\Mailer;
 
 return [
     'components' => [
+        'db' => [
+            'class' => Connection::class,
+            'dsn' => 'mysql:host=127.0.0.1;dbname=yii2_advanced_start',
+            'username' => 'root',
+            'password' => ''
+        ],
         'mailer' => [
             'class' => Mailer::class,
             'viewPath' => '@common/mail'
