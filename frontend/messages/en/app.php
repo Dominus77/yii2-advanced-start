7c802763be5d59615bb879404ac8c92c24be72a4
Aleksey Schevchenko <ivanovosity@gmail.com>
2020-02-21 13:41:31 +0300

    add message i18n

--- frontend/messages/en/app.php
+++ frontend/messages/en/app.php
@@ -7,4 +7,6 @@ return [
     'All rights reserved.' => 'All rights reserved.',
     'The above error occurred while the Web server was processing your request.' => 'The above error occurred while the Web server was processing your request.',
     'Please contact us if you think this is a server error. Thank you.' => 'Please contact us if you think this is a server error. Thank you.',
+    'Maintenance' => 'Maintenance',
+    'The site is undergoing technical work.' => 'The site is undergoing technical work.'
 ];
--- frontend/messages/ru/app.php
+++ frontend/messages/ru/app.php
@@ -7,4 +7,6 @@ return [
     'All rights reserved.' => 'Все права защищены.',
     'The above error occurred while the Web server was processing your request.' => 'Вышеупомянутая ошибка возникла, когда веб-сервер обрабатывал ваш запрос.',
     'Please contact us if you think this is a server error. Thank you.' => 'Если вы считаете, что это ошибка сервера, свяжитесь с нами. Спасибо.',
+    'Maintenance' => 'Техническое обслуживание',
+    'The site is undergoing technical work.' => 'На сайте ведутся технические работы.'
 ];
