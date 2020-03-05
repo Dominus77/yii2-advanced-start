b43dca082366548297849ff9a5085998a484a76c
Aleksey Schevchenko <ivanovosity@gmail.com>
2020-03-05 23:17:06 +0300

    fix debug mode configure

--- environments/dev/common/config/common-local.php
+++ environments/dev/common/config/common-local.php
@@ -3,12 +3,15 @@
 use yii\debug\Module as DebugModule;
 use yii\gii\Module as GiiModule;
 
-if (!YII_ENV_TEST || YII_ENV_DEV) {
-    // configuration adjustments for 'dev' environment
+if (!YII_ENV_TEST && YII_DEBUG) {
+    // configuring in debug mode
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => DebugModule::class
     ];
+}
+if (!YII_ENV_TEST && YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => GiiModule::class
