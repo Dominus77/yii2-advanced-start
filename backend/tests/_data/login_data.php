368e46b1aca97c5c78f58855ffd301abef6e823f
Aleksey Schevchenko <ivanovosity@gmail.com>
2020-05-16 22:52:49 +0300

    fix tests

--- backend/tests/_data/login_data.php
+++ backend/tests/_data/login_data.php
@@ -2,7 +2,7 @@
 return [
     [
         'username' => 'erau',
-        'auth_key' => 'tUu1qHcde0diwUol3xeI-18MuHkkprQI',
+        'auth_key' => 'tUu1qHcde0diwUol3xeI-18MuHkkp1AI',
         // password_0
         'password_hash' => '$2y$13$nJ1WDlBaGcbCdbNC5.5l4.sgy.OMEKCqtDQOdQ2OWpgiKRWYyzzne',
         'password_reset_token' => 'RkD_Jw0_8HEedzLk7MM-ZKEFfYR7VbMr_1392559490',
