891f3704d84edb31614c182785c729ea69aa9b1f
Aleksey Schevchenko <ivanovosity@gmail.com>
2020-06-09 20:55:50 +0300

    fix issue UserQuery.php

--- modules/users/models/query/UserQuery.php
+++ modules/users/models/query/UserQuery.php
@@ -2,6 +2,7 @@
 
 namespace modules\users\models\query;
 
+use yii\db\ActiveQuery;
 use modules\users\models\User;
 
 /**
@@ -9,26 +10,8 @@ use modules\users\models\User;
  *
  * @see \modules\users\models\User
  */
-class UserQuery extends \yii\db\ActiveQuery
+class UserQuery extends ActiveQuery
 {
-    /**
-     * {@inheritdoc}
-     * @return \modules\users\models\User[]|array
-     */
-    public function all($db = null)
-    {
-        return parent::all($db);
-    }
-
-    /**
-     * {@inheritdoc}
-     * @return \modules\users\models\User|array|null
-     */
-    public function one($db = null)
-    {
-        return parent::one($db);
-    }
-
     /**
      * @param int $timeout
      * @return $this
