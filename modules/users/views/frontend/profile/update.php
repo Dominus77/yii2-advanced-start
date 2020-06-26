0f5c05015cd16bf5b53253acc43648cd59163037
Aleksey Schevchenko <ivanovosity@gmail.com>
2020-06-26 16:30:59 +0300

    refactoring

--- modules/users/views/frontend/profile/update.php
+++ modules/users/views/frontend/profile/update.php
@@ -13,38 +13,41 @@ use modules\users\Module;
 $this->title = Module::t('module', 'Update');
 $this->params['breadcrumbs'][] = ['label' => Module::t('module', 'Profile'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
+
+$items = [
+    [
+        'label' => Module::t('module', 'Profile'),
+        'content' => $this->render('tabs/_update_profile', [
+            'model' => $model->profile
+        ]),
+        'options' => ['id' => 'profile', 'role' => 'tabpanel'],
+        'active' => !Yii::$app->request->get('tab') || (Yii::$app->request->get('tab') === 'profile')
+    ],
+    [
+        'label' => Module::t('module', 'Password'),
+        'content' => $this->render('tabs/_update_password', [
+            'model' => $passwordForm
+        ]),
+        'options' => ['id' => 'password', 'role' => 'tabpanel'],
+        'active' => Yii::$app->request->get('tab') === 'password'
+    ],
+    [
+        'label' => Module::t('module', 'Avatar'),
+        'content' => $this->renderFile(Yii::getAlias('@modules/users/views/common/profile/tabs/_update_avatar.php'), [
+            'model' => $model,
+            'uploadFormModel' => $uploadFormModel
+        ]),
+        'options' => ['id' => 'avatar', 'role' => 'tabpanel'],
+        'active' => Yii::$app->request->get('tab') === 'avatar'
+    ]
+];
 ?>
+
 <div class="users-frontend-profile-update">
     <div class="nav-tabs">
         <?= Tabs::widget([
             'options' => ['role' => 'tablist'],
-            'items' => [
-                [
-                    'label' => Module::t('module', 'Profile'),
-                    'content' => $this->render('tabs/_update_profile', [
-                        'model' => $model->profile,
-                    ]),
-                    'options' => ['id' => 'profile', 'role' => 'tabpanel'],
-                    'active' => !Yii::$app->request->get('tab') || (Yii::$app->request->get('tab') === 'profile'),
-                ],
-                [
-                    'label' => Module::t('module', 'Password'),
-                    'content' => $this->render('tabs/_update_password', [
-                        'model' => $passwordForm,
-                    ]),
-                    'options' => ['id' => 'password', 'role' => 'tabpanel'],
-                    'active' => Yii::$app->request->get('tab') === 'password',
-                ],
-                [
-                    'label' => Module::t('module', 'Avatar'),
-                    'content' => $this->renderFile(Yii::getAlias('@modules/users/views/common/profile/tabs/_update_avatar.php'), [
-                        'model' => $model,
-                        'uploadFormModel' => $uploadFormModel
-                    ]),
-                    'options' => ['id' => 'avatar', 'role' => 'tabpanel'],
-                    'active' => Yii::$app->request->get('tab') === 'avatar',
-                ],
-            ]
-        ]); ?>
+            'items' => $items
+        ]) ?>
     </div>
 </div>
